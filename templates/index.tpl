c92f9beb72c7bed1bd7249b5fcc03d06f9f854ac
Abulkanz <alexandre.violeau@outlook.com>
2017-01-17 23:19:09 +0100

    MàJ
    Simplification des exemples
    Ajout de commentaires

--- templates/index.tpl
+++ templates/index.tpl
@@ -1,27 +1,46 @@
 <html>
 <head>
+	<!--Variable de base, chaîne de caractère-->
 	<title>{$titre}</title>
+	<!--Lien entier vers une feuille de style-->
 	{$liensCSS}
 </head>
+<div id='conteneur'>
 <body>
-<h1 id='grosTitre'>Apprendre Smarty avec Patrick Sebastien</h1>
-{$patSeb}
+<!--Titre déclaré de manière conventionnelle-->
+<h1 id='grosTitre'>Apprendre Smarty avec Jean-Claude Van Damme</h1>
+
+<!--Balise image contenant le portrait de Patrick Sebastien-->
+{$jcvd}
 
 
+<!--Entre balises "paragraphe", est aggrémenté du style lui correspondant-->
 <p>
 	{$paragraphe}
 </p>
+<!--Sans balises-->
+{$paragraphe}
+
+<h1>{$titre}</h1>
+
+<!--Boucle à l'interieur d'une table, où l'on insère tour à tour la clef et la valeur des éléments déclarés dans la source PHP-->
 <table>
 {foreach from=$tableau key=k item=i}
 	<tr>
 		<td>{$k}</td>
-		<td>{$titre}</td>
+		
 		<td>{$i}</td>
 	</tr>
 {/foreach}
+<!--Notez la syntae du "foreach"-->
 </table>
-{$paragraphe}</br>{$patSeb}</br>P.{$tableau['Pierre']}
+
+
+</br>{$jcvd}</br>
 <br/>
-{$requete63}
+<!--Resultat de la requête 63-->
+Poids total des friandises = {$requete63} Kg
+
 </body>
+</div>
 </html>
--- templates_c/ebe4ea02b3db4e16395fca1733f10c73c2e78201_0.file.index.tpl.php
+++ templates_c/ebe4ea02b3db4e16395fca1733f10c73c2e78201_0.file.index.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2017-01-15 17:15:28
+/* Smarty version 3.1.30, created on 2017-01-17 22:16:14
   from "C:\wamp64\www\TestSmarty\templates\index.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_587bae3095bc73_12593384',
+  'unifunc' => 'content_587e97ae5d2e45_98729242',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'ebe4ea02b3db4e16395fca1733f10c73c2e78201' => 
     array (
       0 => 'C:\\wamp64\\www\\TestSmarty\\templates\\index.tpl',
-      1 => 1484500526,
+      1 => 1484691371,
       2 => 'file',
     ),
   ),
@@ -20,25 +20,40 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_587bae3095bc73_12593384 (Smarty_Internal_Template $_smarty_tpl) {
+function content_587e97ae5d2e45_98729242 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <html>
 <head>
+	<!--Variable de base, chaîne de caractère-->
 	<title><?php echo $_smarty_tpl->tpl_vars['titre']->value;?>
 </title>
+	<!--Lien entier vers une feuille de style-->
 	<?php echo $_smarty_tpl->tpl_vars['liensCSS']->value;?>
 
 </head>
+<div id='conteneur'>
 <body>
-<h1 id='grosTitre'>Apprendre Smarty avec Patrick Sebastien</h1>
-<?php echo $_smarty_tpl->tpl_vars['patSeb']->value;?>
+<!--Titre déclaré de manière conventionnelle-->
+<h1 id='grosTitre'>Apprendre Smarty avec Jean-Claude Van Damme</h1>
+
+<!--Balise image contenant le portrait de Patrick Sebastien-->
+<?php echo $_smarty_tpl->tpl_vars['jcvd']->value;?>
 
 
 
+<!--Entre balises "paragraphe", est aggrémenté du style lui correspondant-->
 <p>
 	<?php echo $_smarty_tpl->tpl_vars['paragraphe']->value;?>
 
 </p>
+<!--Sans balises-->
+<?php echo $_smarty_tpl->tpl_vars['paragraphe']->value;?>
+
+
+<h1><?php echo $_smarty_tpl->tpl_vars['titre']->value;?>
+</h1>
+
+<!--Boucle à l'interieur d'une table, où l'on insère tour à tour la clef et la valeur des éléments déclarés dans la source PHP-->
 <table>
 <?php
 $_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['tableau']->value, 'i', false, 'k');
@@ -48,8 +63,7 @@ foreach ($_from as $_smarty_tpl->tpl_vars['k']->value => $_smarty_tpl->tpl_vars[
 	<tr>
 		<td><?php echo $_smarty_tpl->tpl_vars['k']->value;?>
 </td>
-		<td><?php echo $_smarty_tpl->tpl_vars['titre']->value;?>
-</td>
+		
 		<td><?php echo $_smarty_tpl->tpl_vars['i']->value;?>
 </td>
 	</tr>
@@ -59,15 +73,19 @@ foreach ($_from as $_smarty_tpl->tpl_vars['k']->value => $_smarty_tpl->tpl_vars[
 $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
 ?>
 
+<!--Notez la syntae du "foreach"-->
 </table>
-<?php echo $_smarty_tpl->tpl_vars['paragraphe']->value;?>
-</br><?php echo $_smarty_tpl->tpl_vars['patSeb']->value;?>
-</br>P.<?php echo $_smarty_tpl->tpl_vars['tableau']->value['Pierre'];?>
 
+
+</br><?php echo $_smarty_tpl->tpl_vars['jcvd']->value;?>
+</br>
 <br/>
-<?php echo $_smarty_tpl->tpl_vars['requete63']->value;?>
+<!--Resultat de la requête 63-->
+Poids total des friandises = <?php echo $_smarty_tpl->tpl_vars['requete63']->value;?>
+ Kg
 
 </body>
+</div>
 </html>
 <?php }
 }
